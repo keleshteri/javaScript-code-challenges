3562d72f389354aae48dca909c40405d7d5b36e9
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-06-11 13:30:53 +0800

    Practice : concatenate-two-strings-in-a-function

---
+++ Practice 1/concatenate-two-strings-in-a-function/index.js
@@ -0,0 +1,9 @@
+let name = "Mehdi"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Mehdi!" when called
+function greetMessage(){
+    console.log(greeting + ", " + name+"!");
+}
+
+greetMessage()
